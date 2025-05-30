35e8a900f1a2dda96c77b4a747f3c59cceca8388
Malak0007 <kiritmaisuriya857@gmail.com>
2025-05-30 17:08:39 +0530

    All without spaceing J patten

--- DSA WITH C++/Patterns/basic.cpp
+++ DSA WITH C++/Patterns/basic.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
   
   for (int i = 1; i <= 5; ++i) { 
-    for (int j = 1; j <= 5; ++j) {
-        cout << i << j << " ";
+    for (int j = 1; j <= 5; ++j){
+      cout << i << j << " ";
     }
-        cout << endl;
+      cout << endl;
   }
   return 0;
 }
